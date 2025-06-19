2e94662c0e26233f33a6db3ea2bd79b312505315
Newton <mwauranewton99@gmail.com>
2025-06-19 15:45:10 +0300

    expecting conflicts

--- helloWorld.zig
+++ helloWorld.zig
@@ -1,4 +1,8 @@
 const std = @import("std");
+
+
 pub fn main() !void{
-    std.debug.print("Hello world in zig language\n",.{});
+    std.debug.print("Hello world in zig language.\nThis language has a very stepp learning curve.\nIt is the safer version of the goat c language\n",.{});
+    std.debug.print("The output is using stderr and not stdout since stderr has a simpler syntax as compared to stdout\n",.{});
+    std.debug.print("Expecting merge conflicts now\n",.{});
 }
