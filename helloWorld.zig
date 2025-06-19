469c261bf3269fa29673bce85dc3477c03608e45
Newton <mwauranewton99@gmail.com>
2025-06-19 16:02:25 +0300

    identation

--- helloWorld.zig
+++ helloWorld.zig
@@ -2,11 +2,12 @@ const std = @import("std");
 
 
 pub fn main() !void{
+
     std.debug.print("Hello world in zig language.\nThis language has a very stepp learning curve.\nIt is the safer version of the goat c language\n",.{});
     std.debug.print("The output is using stderr and not stdout since stderr has a simpler syntax as compared to stdout\n",.{});
     std.debug.print("Expecting merge conflicts now\n",.{});
-pub fn main() !void{
-    std.debug.print("Hello world in zig language.\nThe aouthor in branch irungu\n",.{});
+    pub fn main() !void{
+        std.debug.print("Hello world in zig language.\nThe aouthor in branch irungu\n",.{});
 
 
-}
+    }
