ad936350a14157a987d4be66c0b33611e391f023
newton irungu <mwauranewton99@gmail.com>
2025-09-17 12:08:54 +0300

    reduced the sleep time

--- 30days-challenge/table-formatting.py
+++ 30days-challenge/table-formatting.py
@@ -3,10 +3,10 @@ start = time.perf_counter()
 print("+" + "-" * 10 + "+" + "-" * 15 + "+")
 print("| Header 1 | Header 2      |")
 print("+" + "-" * 10 + "+" + "-" * 15 + "+")
-time.sleep(34)
+time.sleep(10)
 print(start)
 end = time.perf_counter()
 print(end)
 time_taken = end - start
-print(f'It took {time_taken:.4f}s to execute')
+print(f'It took {time_taken:.4f}s to execute.')
 
